03d3fb95ef038b33bafbf20848027b2ff39bde5b
Eduardo Santana Santos <eduardo.ssantos@sptech.school>
2026-04-16 21:30:29 -0300

    Estruturando a API do projeto e criando navbar em um arquivo .js

---
+++ public/js/navbar.js
@@ -0,0 +1,57 @@
+ containerGlobal.innerHTML = `
+           <div class="navLateral">
+            <div class="containerNavLateral">
+                <div class="perfil">
+                    <div class="imgUserNavDiv">
+                        <img src="https://i.pinimg.com/736x/f9/46/92/f94692772d5300dd8964e048b02adcaa.jpg" alt=""
+                            id="imgUserNav">
+                    </div>
+                    <div class="nomeArroba">
+                        <span id="nomeUserNav">Neymar Junior</span>
+                        <span id="usuarioUserNav">@nemidia</span>
+                    </div>
+                </div>
+                <div class="acessosNav">
+                    <div class="acesso">
+                        <i class="fa-regular fa-compass"></i>
+                        <span>Pagina inicial</span>
+                    </div>
+                    <div class="acesso">
+                        <i class="fa-regular fa-lightbulb"></i>
+                        <span>Dicas</span>
+                    </div>
+                    <div class="acesso">
+                        <i class="fa-solid fa-pump-soap"></i>
+                        <span>Produtos</span>
+                    </div>
+                    <div class="acesso">
+                        <i class="fa-solid fa-images"></i>
+                        <span>Galeria</span>
+                    </div>
+                    <div class="acesso">
+                        <i class="fa-solid fa-gear"></i>
+                        <span>Configurações</span>
+                    </div>
+                    <button>Novo post</button>
+                </div>
+            </div>
+        </div>
+        <div class="subContainerGlobal">
+            <div class="navTopo">
+                <img src="./assets/icon/completo.svg" alt="">
+                <div class="navTopoElements">
+                    <div class="inputPesquisar">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="Pesquisar">
+                    </div>
+                </div>
+            </div>
+            <div id="container">
+                ${container.innerHTML}
+            </div>
+             </div>
+        </div>
+
+
+    
+    `
