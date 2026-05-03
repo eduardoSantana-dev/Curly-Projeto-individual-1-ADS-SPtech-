2b8b9b32a30da6c9b347dc75c820a2c2ab443a25
Eduardo Santana Santos <eduardo.ssantos@sptech.school>
2026-05-03 14:49:17 -0300

    Concluindo dashboard

--- public/adm/js/scripts/graficosDash.js
+++ public/adm/js/scripts/graficosDash.js
@@ -11,6 +11,8 @@
                 borderColor: '#00C2FF',
                 tension: 0.4,
                 pointRadius: 2,
+                fill:false,
+                backgroundColor:'#00c3ff1e'
 
             }]
         },
@@ -157,6 +159,90 @@
                     },
                     ticks: {
                         display: false
+                    }
+                }
+            }
+        }
+    });
+
+
+      const linha2 = document.getElementById('graficoLinhaPosts');
+
+    new Chart(linha2, {
+        type: 'line',
+        data: {
+            labels: ['0:00-2:59', '3:00-5:59', '6:00-8:59', '9:00-11:59', '12:00-14:59', '15:00-17:59', '18:00-20:59', '21:00-23:59'],
+            datasets: [{
+                label: 'Posts',
+              data: [8, 5, 18, 42, 58, 74, 95, 70],
+                borderWidth: 2,
+                borderRadius: 3,
+                borderColor: '#00C2FF',
+                backgroundColor: '#00C2FF',
+                tension: 0.2,
+
+            },
+            {
+                label: 'Curtidas',
+                  data: [40, 25, 120, 150, 140, 160, 110, 170],
+                borderWidth: 2,
+                borderRadius: 3,
+                borderColor: '#CB3CFF',
+                backgroundColor: '#CB3CFF',
+          
+                tension: 0.2,
+
+            }
+                ,
+            {
+                label: 'Comentarios',
+                 data: [5, 3, 18, 60, 95, 140, 110, 170],
+                borderWidth: 2,
+                borderRadius: 3,
+                borderColor: '#FFFFFF',
+                backgroundColor: '#FFFFFF',
+    
+                tension: 0.2,
+
+            }
+            ]
+
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: true,
+                   
+                },
+            },
+            scales: {
+                y: {
+
+                    
+                    grid: {
+                        color: '#354155a5',
+                        lineWidth: 1,
+                        drawBorder: false,
+                        drawTicks: false
+                    },
+                    ticks: {
+                        padding: 15,
+                        color: '#fff',
+                    }
+                },
+                x: {
+
+                    grid: {
+                        color: '#354155a5',
+                        lineWidth: 1,
+                        drawBorder: false,
+                        drawTicks: false
+                    },
+                    ticks: {
+                        padding: 5,
+                        color: '#fff',
                     }
                 }
             }
